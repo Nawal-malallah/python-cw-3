7e32beb7ae00f6800a9b4054682cdbde93b023c8
Nawal-malallah <108483676+Nawal-malallah@users.noreply.github.com>
2023-08-28 19:17:59 +0300

    checkpoint

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # write your code here
+favAni = ["cat" , "dog" , "shark" , "sneak"]
+print(favAni)
+print(favAni[1])
+favAni.remove("shark")
+favAni.append("fish")
+
+for n in favAni:
+    print(f"I love {n}")
+
+numbers = [1 , 2 , 3 , 4 , 5]
+numbers_sum = 0
+
+for num in numbers:
+    
